ea9763f133bb4d68815a119c5ee8cf2316779b60
DESKTOP-IEKF7R3\tlgth <tlg.thomaslegall@gmail.com>
2018-01-31 09:01:38 +0100

    Thomas LE GALL
    TP - Sécurité

--- webcryptoapi.js
+++ webcryptoapi.js
@@ -1,4 +1,5 @@
 (function() {
+
 	function str2ab16(str) {
 		var ab = new ArrayBuffer(str.length * 2);
 		var abView = new Uint16Array(ab);
@@ -34,22 +35,110 @@
 	}
 
 	Cypher = function() {
-		this.key = null;
-		//this.iv = window.crypto.getRandomValues(new Uint8Array(16));
+
+		/*------- Automatic iv ---------*/
+		var password = "";
+		do{
+			password = window.prompt("Choose a password less than 8 characters");
+			console.log(password.length);
+			if(password.length < 8){
+				do{
+					password = "0"+password;
+				}while(password.length < 8)
+			}
+		}while(password.length > 8)
+		password = str2ab16(password);
+		this.iv = password;
+
+		/*------- Manual iv ---------*/
+		/*this.iv = window.crypto.getRandomValues(new Uint8Array(16));*/
+
+		this.algo = {name : "AES-CBC", iv : this.iv};
 	}
 
 	Cypher.prototype = {
-		importKey : function (success, err) {
-			
+		importKey : function (success, error) {
+			// var password = window.prompt("Choose password ?");
+			var self = this;
+			window.crypto.subtle.digest(
+			{
+				name: "SHA-256",
+			},
+			this.iv // La donnée type source
+			)
+			.then(function(hash) {
+				window.crypto.subtle.importKey(
+					"raw", // format de la clef
+					hash,
+					self.algo,
+					false,
+					["encrypt", "decrypt"]
+				).then(function(key){
+				 	
+				 	success(key);
+				})
+				.catch(function(err){
+					console.error(err);
+				});
+			}).catch(function(err) {
+				console.error(err);
+			})
 		},
 
 		encrypt : function (text, success, err) {
-			
+			var self = this;
+			this.importKey(function(key) {
+				window.crypto.subtle.encrypt(
+					self.algo, // Paramètres d'identification de l'algorithme cible
+					key, // clef au format interne générée par generateKey ou importKey
+					str2ab16(self.iv+"|"+text) // Donnée claire au format ArrayBuffer
+				)
+				.then(function(encrypted){
+				// encrypted sont les données chiffrées sous forme d'ArrayBuffer
+
+					
+					var b64 = window.btoa(ab82str(encrypted));
+					success(b64);
+
+				})
+				.catch(function(err){
+					console.error(err);
+				});
+			}),
+			function(err){
+				console.log(err);
+			}
 		},
 		
-		decrypt : function (data, success, err) {
-			
-		}		
+		decrypt : function (text, success, err) {
+			var self = this;
+			this.importKey(function(key) {
+				window.crypto.subtle.decrypt(
+					self.algo, 
+					key, 
+					str2ab8(window.atob(text))
+				)
+				.then(function(decrypted){
+					/*Données à déchiffrer*/
+					console.log(decrypted);
+					var b64Decode = ab162str(decrypted);
+					/*Deux parties iv|chaine*/
+					console.log(str2ab16(b64Decode.split("|")[0]) );/*
+					if(!= str2ab16(self.vi)){
+						alert("WRONG...");
+					}*/
+					success(b64Decode.split("|")[1]);
+
+				})
+				.catch(function(err){
+					alert("WRONG PASSWORD");
+					console.error(err);
+				});
+			}),
+			function(err){
+				console.log(err);
+			}
+		},	
 	}
 })();
 
